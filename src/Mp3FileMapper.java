cad36e8738cf5f7cd339f1749da459bf8bc4176c
이상현 <dkrdj1000@naver.com>
2023-03-22 13:59:26 +0900

    1

--- src/Mp3FileMapper.java
+++ src/Mp3FileMapper.java
@@ -7,7 +7,6 @@ import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 
 import java.io.IOException;
-import java.io.InputStream;
 
 public class Mp3FileMapper extends Mapper<Object, Text, Text, Text> {
 
@@ -18,15 +17,15 @@ public class Mp3FileMapper extends Mapper<Object, Text, Text, Text> {
         FileSystem fs = FileSystem.get(conf);
         // HDFS에서 파일 경로 가져오기
         Path path = new Path(value.toString());
-        InputStream inputStream = fs.open(path);
+//        InputStream inputStream = fs.open(path);
 
         // .mp3 파일을 바이트 배열로 변환하여 맵리듀스 출력으로 내보내기
-        byte[] bytes = new byte[inputStream.available()];
-        inputStream.read(bytes);
+//        byte[] bytes = new byte[inputStream.available()];
+//        inputStream.read(bytes);
         context.write(new Text(key.toString()), new Text(value.toString()));
 //        context.write(value, new BytesWritable(bytes));
 
         // 입력 스트림 닫기
-        inputStream.close();
+//        inputStream.close();
     }
 }
--- src/Mp3ToWavJob.java
+++ src/Mp3ToWavJob.java
@@ -8,6 +8,7 @@ import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.NullOutputFormat;
 import org.apache.hadoop.util.Tool;
 import org.apache.hadoop.util.ToolRunner;
 
@@ -32,9 +33,11 @@ public class Mp3ToWavJob extends Configured implements Tool {
         job.setJarByClass(Mp3ToWavJob.class);
         job.setMapperClass(Mp3FileMapper.class);
         job.setInputFormatClass(Mp3InputFormat.class);
+
         job.setOutputKeyClass(Text.class);
         job.setOutputValueClass(Text.class);
         job.setNumReduceTasks(0);
+        job.setOutputFormatClass(NullOutputFormat.class);
 
         FileInputFormat.setInputPaths(job, new Path(args[0]));
         FileOutputFormat.setOutputPath(job, new Path(args[1]));
