4015d983c807ff52818c71348a3e4c55ab16c4f6
이상현 <dkrdj1000@naver.com>
2023-03-22 14:12:46 +0900

    1

--- src/Reducerer.java
+++ src/Reducerer.java
@@ -7,6 +7,9 @@ import java.io.IOException;
 
 public class Reducerer extends Reducer<Text, Text, Text, Text> {
     public void reduce(Text key, Text value, Context context) throws IOException, InterruptedException {
+        if (key.toString().equals("") || key.toString() == null) {
+            throw new RuntimeException("뭔가 잘못됨");
+        }
         context.write(key, value);
     }
 }
