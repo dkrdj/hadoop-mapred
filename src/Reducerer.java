56bf6cdb725fa83b56ebb2051d1470d456225ce9
이상현 <dkrdj1000@naver.com>
2023-03-22 17:26:17 +0900

    1

--- src/Reducerer.java
+++ src/Reducerer.java
@@ -2,13 +2,14 @@ package ssafy;
 
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.BytesWritable;
+import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
 
 import java.io.IOException;
 
-public class Reducerer extends Reducer<Path, BytesWritable, Path, BytesWritable> {
+public class Reducerer extends Reducer<Path, BytesWritable, Path, Text> {
 
     public void reduce(Path key, BytesWritable value, Context context) throws IOException, InterruptedException {
-        context.write(key, value);
+        context.write(key, new Text("1231"));
     }
 }
