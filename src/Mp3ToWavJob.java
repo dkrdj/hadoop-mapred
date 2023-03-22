593f933cf0bd6ba466673947c39c786b5f47b85e
이상현 <dkrdj1000@naver.com>
2023-03-22 14:10:29 +0900

    1

--- src/Mp3ToWavJob.java
+++ src/Mp3ToWavJob.java
@@ -1,40 +1,33 @@
 package ssafy;
 
 import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.conf.Configured;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
-import org.apache.hadoop.util.Tool;
-import org.apache.hadoop.util.ToolRunner;
+import org.apache.hadoop.util.GenericOptionsParser;
 
-public class Mp3ToWavJob extends Configured implements Tool {
+public class Mp3ToWavJob {
 
     public static void main(String[] args) throws Exception {
-        int exitCode = ToolRunner.run(new Configuration(), new Mp3ToWavJob(), args);
-        System.exit(exitCode);
-    }
 
-    @Override
-    public int run(String[] args) throws Exception {
-
-        if (args.length != 2) {
-            System.err.println("Usage: Mp3ToWavJob <input path> <output path>");
-            return -1;
+        Configuration conf = new Configuration();
+        String[] otherArgs = new GenericOptionsParser(conf, args).getRemainingArgs();
+        if (otherArgs.length != 2) {
+            System.err.println("Usage: <in> <out>");
+            System.exit(2);
         }
 
-        Configuration conf = getConf();
-        Job job = Job.getInstance(conf, "Mp3 to Wav conversion job");
+        Job job = new Job(conf, "Mp3ToWav");
 
         job.setJarByClass(Mp3ToWavJob.class);
         job.setMapperClass(Mp3FileMapper.class);
         job.setInputFormatClass(Mp3InputFormat.class);
 
+        job.setReducerClass(Reducerer.class);
         job.setOutputKeyClass(Text.class);
         job.setOutputValueClass(Text.class);
-        job.setReducerClass(Reducerer.class);
         job.setNumReduceTasks(1);
 
         FileInputFormat.setInputPaths(job, new Path(args[0]));
@@ -45,6 +38,6 @@ public class Mp3ToWavJob extends Configured implements Tool {
 //            fs.delete(new Path(args[1]), true);
 //        }
 
-        return job.waitForCompletion(true) ? 0 : 1;
+        System.exit(job.waitForCompletion(true) ? 0 : 1);
     }
 }
