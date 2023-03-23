96073584d92b4efaebef55c25794b754a157bbd8
이상현 <dkrdj1000@naver.com>
2023-03-23 10:59:38 +0900

    1

--- src/Wordcount.java
+++ src/Wordcount.java
@@ -102,7 +102,7 @@ public class Wordcount {
 //                localOutput.close();
 //
                 //파이썬 코드 실행
-                ProcessBuilder pb = new ProcessBuilder("python", "test.py");
+                ProcessBuilder pb = new ProcessBuilder("python3", "test.py");
                 pb.redirectErrorStream(true);
                 Process process = pb.start();
                 int exitval = process.waitFor();
